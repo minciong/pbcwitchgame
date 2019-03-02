87720a8f859bf28ef79d87ab5c1d7ef8b2d0f4e4
minciong <maxraphael_inciong@yahoo.com>
2019-03-02 13:57:02 -0500

    have stuff

---
+++ Assets/GoombaController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaController : MonoBehaviour {
+	protected Vector3 direction = Vector3.right;
+	protected float movespeed = 2.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + direction * Time.deltaTime * movespeed;
+		if(WallDistance()<0.5f){
+			direction = -direction;
+		}
+	}
+	
+	float WallDistance () {
+		var results = new RaycastHit2D[1];
+		var count = GetComponent<Collider2D>().Raycast(direction, results);
+		if(count < 1){
+			return 1000;
+		}
+		else{
+			return results[0].distance;
+		}
+		
+	}
+	
+	public void OnCollisionEnter (Collision c){
+		
+		var player = c.collider.GetComponent<PlayerController>();
+		if(player != null){
+			Destroy(player.gameObject);
+		}
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	protected float movespeed = 5f;
+	protected float jumpspeed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("floordistance: "+ FloorDistance());
+		if(Input.GetKey(KeyCode.A)){
+			transform.position = transform.position - Vector3.right * Time.deltaTime * movespeed;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.position = transform.position + Vector3.right * Time.deltaTime * movespeed;
+		}
+		if(Input.GetKeyDown(KeyCode.W) && FloorDistance() < 1.2f){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0,jumpspeed);
+			
+		}
+		if(Input.GetKey(KeyCode.S)){
+			
+		}
+	}
+	float FloorDistance () {
+		var results = new RaycastHit2D[1];
+		var count = GetComponent<Collider2D>().Raycast(-Vector2.up, results);
+		if(count < 1){
+			return 1000;
+		}
+		else{
+			return results[0].distance;
+		}
+		
+	}
+}
