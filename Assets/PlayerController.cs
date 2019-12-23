e1e0765d3a08a8f37f2cd82ed3b944c24ea34367
Jade <f.truncale0@protonmail.ch>
2019-12-23 02:22:40 -0500

    Using Unity's physics engine for movement Changed everything to add force to the RigidBody on input, rather than forcing transformation.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PlayerController : MonoBehaviour {
-	protected float jumpSpeed = 10f;
-    protected float xVel = 0f; //stores horizontal current velocity
-    protected float accel = 0.04f; //value of increased speed for chosen direction
-    protected float xStopRate = 0.1f; //rate to slow down at when not inputting
-    protected float xStopRange = 2f; //how close to this value in or der to full stop
-    protected float xMaxVel = 4f; //maximum x velocity allowed
-    protected float sprintMult = 1.5f; //sprint multiplier
-    protected float sprintVal = 1f; //how much sprint currently
-    protected float yVel = 0f; //y velocity
-    protected float duckRate = 0.1f; //negative y velocity from ducking mid-air
+	private Rigidbody2D playerBody;
+	protected float jumpSpeed = 50f;
+	protected float accel = 5f; //value of increased speed for chosen direction
+	protected float xMaxVel = 6f; //maximum x velocity allowed
+	protected float sprintMult = 1.5f; //sprint multiplier
+	protected float sprintVal = 1f; //how much sprint currently
+	protected float duckRate = 10f; //negative y velocity from ducking mid-air
 	protected Sprite[] movesprites;
 	protected Sprite standsprite;
 	protected Sprite jumpsprite;
@@ -24,67 +22,84 @@ public class PlayerController : MonoBehaviour {
 		standsprite = Resources.Load<Sprite>("Sprites/mario_stand");
 		jumpsprite = Resources.Load<Sprite>("Sprites/mario_jumping");
 		ducksprite = Resources.Load<Sprite>("Sprites/mario_crouch");
+
+		playerBody = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
-	
+
 	void Update () {
 		var isMoving = false;
 		var isDucking = false;
-		//Debug.Log("floordistance: "+ FloorDistance());
+		var xForce = 0f;
+		var yForce = 0f;
+		var forceStop = 1f;
+		var duckVal = 0f;
+
+		if(Input.GetButtonDown("Sprint")){
+			sprintVal = sprintMult;
+	    xMaxVel += 2;
+	  }
+
+		if(Input.GetButtonUp("Sprint")){
+			sprintVal = 1;
+			xMaxVel -= 2;
+		}
+
 		if(Input.GetButton("Down")){
 			isDucking = true;
-            yVel -= duckRate;
-            
+	    duckVal -= duckRate;
 		}
-        if(Input.GetButtonDown("Sprint")){
-			sprintVal = sprintMult;
-            xMaxVel += 2;
-        }
-        else if(Input.GetButtonUp("Sprint")){
-			sprintVal = 1;
-            xMaxVel -= 2;
-        }
-        if(Input.GetButton("Left") && transform.position.x >= Camera.main.transform.position.x - 6){
+
+	  if(Input.GetButton("Left") && transform.position.x >= Camera.main.transform.position.x - 6){
 			isMoving = true;
 			transform.localScale = new Vector3(-1,1,1);
-            if (xVel >= (xMaxVel * -1)){ xVel -= accel; }
+	          if (playerBody.velocity.x >= (xMaxVel * -1)){
+							xForce += (-1)*accel;
+						}
 		}
 		if(Input.GetButton("Right")){
 			isMoving = true;
-            if (xVel <= xMaxVel){ xVel += accel; } 
+	          if (playerBody.velocity.x <= xMaxVel){
+							 xForce += accel;
+						 }
 			transform.localScale = new Vector3(1,1,1);
 		}
+
 		if(Input.GetButton("Jump") && FloorDistance() < 1.2f){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(0,jumpSpeed);
-			
+			yForce = jumpSpeed;
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(0,jumpSpeed);
+
 		}
-				
+
+		if(FloorDistance() >= 1.2f){
+			GetComponent<SpriteRenderer>().sprite = jumpsprite;
+		}
+
 		if(isDucking){
 			GetComponent<SpriteRenderer>().sprite = ducksprite;
-            yVel -= duckRate;
-		}else if(FloorDistance() >= 1.2f){
-			GetComponent<SpriteRenderer>().sprite = jumpsprite;
-        }if(FloorDistance() >= 0.05){
-			yVel = 0;
-		}if(isMoving){
+			yForce = (-1)*duckRate;
+		}
+
+		if(isMoving){
 			animationTimer += Time.deltaTime;
 			animationTimer %= 0.3f;
 			GetComponent<SpriteRenderer>().sprite = movesprites[(int)(animationTimer/0.1f)];
 		}
-		else{
-            if (xVel > xStopRange) //if we've got positive xVel, slow down
-                xVel -= xStopRate;
-            else if ((xVel < xStopRange && xVel > 0) || (xVel > (xStopRange * -1) && xVel < 0)) //if we're close enough to stop, stop
-                xVel = 0; 
-            else if (xVel < 0) //if negative xVel, increase
-                xVel += xStopRate;
-            
+		else {
 			GetComponent<SpriteRenderer>().sprite = standsprite;
 		}
-        transform.position = transform.position + (new Vector3(xVel, yVel, 0) * sprintVal) * Time.deltaTime;
+				Debug.Log("xForce: "+ xForce);
+				Debug.Log("xMaxVel: "+ xMaxVel);
+				Debug.Log("xStopRate: "+ xStopRate);
+				Debug.Log("forceStop: "+ forceStop);
+				Debug.Log("Calculated xForce:" +  (xForce*sprintVal) * forceStop);
+				playerBody.AddForce(new Vector2( ( (xForce*sprintVal) * forceStop ), yForce + duckVal) );
+				forceStop = 1f;
+				duckVal = 0f;
+	      //transform.position = transform.position + (new Vector3(xVel, yVel, 0) * sprintVal) * Time.deltaTime;
 
-		
+
 	}
 	float FloorDistance () {
 		var results = new RaycastHit2D[1];
@@ -95,7 +110,7 @@ public class PlayerController : MonoBehaviour {
 		else{
 			return results[0].distance;
 		}
-		
+
 	}
 	public void OnKillPlayer(){
 		SceneManager.LoadScene("GameOverScene");
