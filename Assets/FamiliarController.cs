a9d592dd300d2dc9670b73b75e24e9c2c359f5ce
minciong <maxraphael_inciong@yahoo.com>
2019-12-22 20:21:39 -0500

    orbit achieved

--- Assets/FamiliarController.cs
+++ Assets/FamiliarController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FamiliarController : MonoBehaviour
 {
+    protected float radius=3;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,9 @@ public class FamiliarController : MonoBehaviour
         // Debug.Log(transform.position);
         var witchPos = transform.parent.gameObject.transform.position;
         var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mouse.x, mouse.y, 0);
+        
+        var direction = new Vector3(mouse.x, mouse.y, 0)-witchPos;
+        transform.position = witchPos+Vector3.Normalize(direction)*radius;
         // Debug.Log(transform.parent.gameObject.transform.position);
         // Debug.Log(distance);
     }
