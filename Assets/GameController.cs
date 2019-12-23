7a959b814bccc77ac6c47cf8da735353915baf1d
Jade <f.truncale0@protonmail.ch>
2019-12-23 17:06:12 -0500

    Fixed bugs from merge.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -7,22 +7,17 @@ public class GameController : MonoBehaviour {
 	public Text scoreText;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		var v3 = Input.mousePosition;
 		v3 = Camera.main.ScreenToWorldPoint(v3);
-<<<<<<< HEAD
-		//Debug.Log(v3);
-=======
-// 		Debug.Log(v3);
->>>>>>> witch_movement
 		scoreText.text = "Score: " + score;
 	}
 	public void UpdateScore(int value){
 		score+=value;
 	}
-	
+
 }
