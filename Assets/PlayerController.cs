f4b93c3adaaf77f6c994758b4f8c965d254569ae
Jade <f.truncale0@protonmail.ch>
2019-12-30 18:02:48 -0500

    Made physics actions independent of framerate (fixedUpdate())

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
-	private SpriteRenderer playerSpriteRenderer;
-	private Rigidbody2D playerBody;
-	public float jumpSpeed = 100f;
-	public float accel = 6f; //value of increased speed for chosen direction
-	public float xMaxVel = 7f; //maximum x velocity allowed
+	protected private SpriteRenderer playerSpriteRenderer;
+	protected private Rigidbody2D playerBody;
+	public float jumpSpeed = 200f;
+	public float accel = 12f; //value of increased speed for chosen direction
+	public float xMaxVel = 8f; //maximum x velocity allowed
 	public float sprintMult = 1.5f; //sprint multiplier
 	public float sprintVal = 1f; //how much sprint currently
-	public float duckRate = 10f; //negative y velocity from ducking mid-air
+	public float duckRate = 20f; //negative y velocity from ducking mid-air
 	public float animationTimer = 0;
 	protected Sprite[] moveSprites;
 	protected Sprite standSprite;
@@ -28,54 +28,71 @@ public class PlayerController : MonoBehaviour {
 		playerBody = GetComponent<Rigidbody2D>();
 	}
 
-	// Update is called once per frame
-	void Update () {
-		var isMoving = false;
-		var isDucking = false;
+	//FixedUpdate works independent of frame rate, for interaction with the physics system
+	void FixedUpdate () {
+
 		var xForce = 0f; //these are to be appied to the playerBody before the function ends
 		var yForce = 0f;
 		var duckVal = 0f;
 
-		if(Input.GetButtonDown("Sprint")){
-			sprintVal = sprintMult;
-	    xMaxVel += 2;
-	  }
-
-		if(Input.GetButtonUp("Sprint")){
-			sprintVal = 1;
-			xMaxVel -= 2;
-		}
-
 		if(Input.GetButton("Down")){
-			isDucking = true;
-	    duckVal -= duckRate;
+			duckVal -= duckRate;
+			yForce = (-1)*duckRate; //add some negative force when trying to go down
 		}
 
-	  if (Input.GetButton("Left") && transform.position.x >= Camera.main.transform.position.x - 6){
-			isMoving = true;
-			transform.localScale = new Vector3(-1,1,1);
-	          if (playerBody.velocity.x >= (xMaxVel * -1)){
-							xForce += (-1)*accel;
-						}
+		if (Input.GetButton("Left")){
+			if (playerBody.velocity.x >= (xMaxVel * -1)){
+				xForce += (-1)*accel;
+			}
 		}
+
 		if (Input.GetButton("Right")){
-			isMoving = true;
-	          if (playerBody.velocity.x <= xMaxVel){
-							 xForce += accel;
-						 }
-			transform.localScale = new Vector3(1,1,1);
+			if (playerBody.velocity.x <= xMaxVel){
+				 xForce += accel;
+			 }
+
 		}
 
 		if (Input.GetButton("Jump") && FloorDistance() < 1.2f){
 			yForce = jumpSpeed;
 		}
 
-		if (isDucking){
-			yForce = (-1)*duckRate; //add some negative force when trying to go down
+		playerBody.AddForce(new Vector2( ( xForce*sprintVal ), yForce + duckVal) ); //apply all force as caluclated above
+		duckVal = 0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var isMoving = false;
+		var isDucking = false;
+
+		if(Input.GetButtonDown("Sprint")){
+			sprintVal = sprintMult;
+			xMaxVel += 2;
+		}
+
+		if(Input.GetButtonUp("Sprint")){
+			sprintVal = 1;
+			xMaxVel -= 2;
+		}
+
+		//BEGIN: sprite logic
+
+		if (Input.GetButton("Left")){
+			isMoving = true;
+			transform.localScale = new Vector3(-1,1,1); //flip sprite left
+		}
+
+		if (Input.GetButton("Right")){
+			isMoving = true;
+			transform.localScale = new Vector3(1,1,1);
 		}
 
 
-		//BEGIN: sprite logic
+		if(Input.GetButton("Down")){
+			isDucking = true;
+		}
+
 		if (isDucking){ //ducking overrides other sprites
 			playerSpriteRenderer.sprite = duckSprite;
 		}
@@ -95,9 +112,6 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		//END: sprite logic
-
-		playerBody.AddForce(new Vector2( ( xForce*sprintVal ), yForce + duckVal) ); //apply all force as caluclated above
-		duckVal = 0f;
 
 	}
 
