3cc5a029985f481da22e8fa6fe35702432416399
Jade <f.truncale0@protonmail.ch>
2020-01-20 21:49:31 -0500

    Allow drawing shield with Mouse 1 Fixed familiar and player collision Needs collider still

---
+++ Assets/ShieldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour
+{
+    public GameObject ColliderPrefab;
+
+    protected TrailRenderer trail;
+    // Start is called before the first frame update
+    void Start()
+    {
+      trail = GetComponent<TrailRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
