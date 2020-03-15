add713f7d4a76ad2f47849e9d430d3972a405cc4
minciong <maxraphael_inciong@yahoo.com>
2020-03-15 12:37:53 -0400

    basic projectiles

---
+++ Assets/Bolt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+	public float speed = 10f;
+	public Rigidbody2D rb ;
+	public float bulletgrowth = 10f;
+	public float lifetime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right*speed;
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     	transform.localScale += new Vector3(bulletgrowth*Mathf.Sin(Time.deltaTime),
+     										bulletgrowth*Mathf.Sin(Time.deltaTime),
+     										bulletgrowth*Mathf.Sin(Time.deltaTime));  
+
+    }
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform Castpoint;
+    public GameObject boltPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetButtonDown("Fire1")){
+    		Shoot();
+    	}   
+    }
+    void Shoot(){
+    	Instantiate(boltPrefab,Castpoint.position,Castpoint.rotation);
+    }
+}
