f1983f24a23da3ba2580ad22e04ffa42df4a7381
minciong <maxraphael_inciong@yahoo.com>
2020-04-09 21:11:09 -0400

    added Paul's pause button.

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    bool paused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause")){
+            paused = !paused;
+        }
+        if(paused){
+            Time.timeScale = 0;
+            GetComponent<Weapon>().enabled = false;
+            GetComponent<PlayerController>().enabled = false;
+            GameObject.Find("Familiar").GetComponent<FamiliarController>().enabled = false;
+        } else {
+            Time.timeScale = 1;
+            GetComponent<Weapon>().enabled = true;
+            GetComponent<PlayerController>().enabled = true;
+            GameObject.Find("Familiar").GetComponent<FamiliarController>().enabled = true;
+        }
+    }
+}
