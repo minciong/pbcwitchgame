250e027a8933619e2124308665f3c2c59e97275b
minciong <maxraphael_inciong@yahoo.com>
2020-05-10 14:04:11 -0400

    new smoky projectiles

--- Assets/Bolt.cs
+++ Assets/Bolt.cs
@@ -7,11 +7,16 @@ public class Bolt : GenericController{
 	public Rigidbody2D rb ;
 	public float bulletgrowth = 10f;
 	public float lifetime = 1f;
+  public Sprite[] bulletSprites;
+  protected SpriteRenderer boltSprite;
+  protected float animationTimer = 0;
+
   // Start is called before the first frame update
   void Start()
   {
 			this.damage = 1;
-
+      bulletSprites = Resources.LoadAll<Sprite>("Sprites/SmokeBullet");
+      boltSprite = GetComponent<SpriteRenderer>();
       rb.velocity = transform.right;
       if(Input.GetButton("Up")){
       	rb.velocity += new Vector2(0,1);
@@ -26,6 +31,10 @@ public class Bolt : GenericController{
   // Update is called once per frame
   void Update()
   {
+    // boltPrefab.Sprite = bulletSprites;
+    animationTimer += Time.deltaTime;
+    animationTimer %= lifetime;
+    boltSprite.sprite = bulletSprites[(int)(animationTimer/(lifetime/5))];
    	transform.localScale += new Vector3(bulletgrowth* Time.deltaTime,
    										bulletgrowth* Time.deltaTime,
    										bulletgrowth* Time.deltaTime);
